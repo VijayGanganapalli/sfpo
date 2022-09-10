b4b106a2085ef907ebb0701212eaa16ffbe920ec
Vijay <vijayganganapalli@gmail.com>
2022-09-10 05:58:25 +0530

    added sliver appbar

--- lib/screens/business_screen.dart
+++ lib/screens/business_screen.dart
@@ -1,13 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:sfpo/constants/packages.dart';
 
-class BusinessScreen extends StatelessWidget {
+class BusinessScreen extends StatefulWidget {
   const BusinessScreen({Key? key}) : super(key: key);
 
   @override
+  State<BusinessScreen> createState() => _BusinessScreenState();
+}
+
+class _BusinessScreenState extends State<BusinessScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("There is no business"),
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar.medium(
+            title: Text('Business', style: TextStyle(color: accentColor)),
+            actions: [
+              IconButton(
+                color: accentColor,
+                icon: Icon(Icons.more_vert),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
