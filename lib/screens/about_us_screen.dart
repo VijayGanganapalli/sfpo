d0632b57016b266fcd56e037c8b664559b8192d4
Vijay <vijayganganapalli@gmail.com>
2022-09-10 07:48:10 +0530

    added sliver appbar

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -1,13 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:sfpo/constants/packages.dart';
 
 class AboutUsScreen extends StatelessWidget {
   const AboutUsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("We will implement this screen soon"),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.medium(
+            title: Text('About us', style: TextStyle(color: accentColor)),
+          ),
+        ],
       ),
     );
   }
